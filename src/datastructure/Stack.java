59edf0b14c4c8ff8e2b4d241568f316c763052e8
Amir <44071527+amirahkami@users.noreply.github.com>
2024-08-31 14:36:52 +0200

    added stack data structure

--- src/datastructure/Stack.java
+++ src/datastructure/Stack.java
@@ -2,4 +2,54 @@ package datastructure;
 
 public class Stack
 {
+    private char[] stack;
+    private int top;
+    private int size;
+
+    public Stack(int size)
+    {
+        this.size = size;
+        this.stack = new char[size];
+        this.top = -1;
+    }
+
+    public void push(char element)
+    {
+        if (top == size - 1) {
+            System.out.println("Stack is full!");
+            return;
+        }
+
+        stack[++top] = element;
+    }
+
+    public char pop()
+    {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return ' ';
+        }
+
+        return stack[top--];
+    }
+
+    public char peek()
+    {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return ' ';
+        }
+
+        return stack[top];
+    }
+
+    public boolean isEmpty()
+    {
+        return top == -1;
+    }
+
+    public int getSize()
+    {
+        return top + 1;
+    }
 }
